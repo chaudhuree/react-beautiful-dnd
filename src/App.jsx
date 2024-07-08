9d033bb5f6ee6c59a7390becac37a1e2b520ae24
chaudhuree <chaudhuree00@gmail.com>
2024-07-09 00:08:52 +0600

    fix: add DragDropContext in the App.jsx because main.jsx has issues with onDragEnd handler function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
+  const handleOnDragEnd = (result) => {
+    console.log(result);
+    const { source, destination } = result;
+  };
   return (
-    <div className="p-4 bg-gray-600 text-sky-400">
-    {/*
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <div className="p-4 bg-gray-600 text-sky-400">
+        {/*
       header
     */}
-      <h1 className="w-2/4 text-center mx-auto text-4xl font-extrabold my-5">
-        Drag an Drop
-      </h1>
-      {/*
+        <h1 className="w-2/4 text-center mx-auto text-4xl font-extrabold my-5">
+          Drag an Drop
+        </h1>
+        {/*
         drag and drop area
       */}
-      <div className="flex justify-between px-4">
-        <Column />
-        <Column />
-        <Column />
+        <div className="flex justify-between px-4">
+          <Column />
+          <Column />
+          <Column />
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { DragDropContext } from "react-beautiful-dnd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DragDropContext>
-      <App />
-    </DragDropContext>
+    <App />
   </React.StrictMode>
 );
