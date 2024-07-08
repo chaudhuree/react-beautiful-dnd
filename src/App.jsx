85ca1f0218f0b6985e957969f0c1154c0b34bea0
chaudhuree <chaudhuree00@gmail.com>
2024-07-09 00:02:03 +0600

    basic design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Column from "./Column";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="p-4 bg-gray-600 text-sky-400">
+    {/*
+      header
+    */}
+      <h1 className="w-2/4 text-center mx-auto text-4xl font-extrabold my-5">
+        Drag an Drop
+      </h1>
+      {/*
+        drag and drop area
+      */}
+      <div className="flex justify-between px-4">
+        <Column />
+        <Column />
+        <Column />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Column.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Column() {
+  return (
+    <div className="flex flex-col w-[350px] min-h-[620px] bg-gray-700 font-bold rounded-md border  border-gray-900">
+          <div className="flex rounded-t-md items-center text-xl justify-center h-16 py-1 mb-2 bg-gray-800">
+            TODO
+          </div>
+        </div>
+  )
+}
